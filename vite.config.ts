bb0b9dfe10b73cde80203b1e07a5b294cb1f85a6
hipuku <hipuku.dev@gmail.com>
2026-06-03 08:02:29 +1000

    Point @kern alias to installed kern package from GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@kern': path.resolve(__dirname, '../kern/src'),
+      '@kern': path.resolve(__dirname, 'node_modules/kern/src'),
     },
   },
   test: {
